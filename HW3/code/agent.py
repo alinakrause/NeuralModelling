96153fb9b6f85603811268d8638073e101166dfb
alinakrause <76391336+alinakrause@users.noreply.github.com>
2023-12-19 15:51:31 +0100

    task 2 change

--- HW3/code/agent.py
+++ HW3/code/agent.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy import stats # for gaussian noise
-from environment import Environment
+from environment import Environment, Environment_TwoStepAgent
 import random
 
 
@@ -196,7 +196,8 @@ class DynaAgent(Environment):
         # complete the code
         for i in range(num_planning_updates):
             #num states
-            rows = self.experience_buffer.shape[0]
+            rows = len(self.experience_buffer)
+
             # pick a state
             select_ind = np.random.randint(rows)
             s,a,r,s1 = self.experience_buffer[select_ind]
@@ -258,7 +259,7 @@ class DynaAgent(Environment):
 
         return None
     
-class TwoStepAgent:
+class TwoStepAgent(Environment_TwoStepAgent):
 
     def __init__(self, alpha1, alpha2, beta1, beta2, lam, w, p):
 
@@ -282,8 +283,41 @@ class TwoStepAgent:
         self.w      = w
         self.p      = p
 
+        self.rewards = np.random.uniform(0.25,0.75,4)
+
+
+        self.transition_matrix = [[0.7,0.3], [0.3,0.7]]
+        self.transition = self.transition_matrix[0] 
+        self.transition_count_b = 0
+        self.transition_count_c = 0
+
+        self.last_a = -1
+
         return None
         
+    def init_env(self, env_config):
+
+        '''
+        Initialise the environment
+        Input arguments:
+            **env_config -- dictionary with environment parameters
+        '''
+        Environment_TwoStepAgent.__init__(self, env_config)
+        return None
+    
+    
+    def _init_q_values(self):
+
+        '''
+        Initialise the Q-value table
+        '''
+
+        self.QTD = np.zeros((self.num_states* self.num_actions))
+        self.QMB = np.zeros((self.num_states* self.num_actions))
+        self.Qnet = np.zeros((self.num_states* self.num_actions))
+        return None
+    
+
     def _init_history(self):
 
         '''
@@ -294,20 +328,138 @@ class TwoStepAgent:
 
         return None
     
-    def _update_history(self, a, s1, r1):
+    def _update_history(self, a, s1, r):
 
         '''
-        Update history
+        Update the history
         Input arguments:
-            a  -- first stage action
-            s1 -- second stage state
-            r1 -- second stage reward
+            s     -- initial state
+            a     -- chosen action
+            r     -- received reward
+            s1    -- next state
         '''
 
-        self.history = np.vstack((self.history, [a, s1, r1]))
+        self.history = np.vstack((self.history, np.array([a, s1, r])))
+        return None
+
+
+    def add_noise(self):
+        '''
+        Update rewards using a Gaussian random walk and keep them within boundaries.
+        '''
+        # Update rewards with a Gaussian random walk
+        self.rewards += np.random.normal(loc=0, scale=0.025, size=4)
+        self.rewards = np.clip(self.rewards, 0.25, 0.75)
+        return None
+
+
+
+
+    def _Q_td(self, s, a, r, s1, a1, trace):
+
+        '''
+        Update the Q-value table of td
+        Input arguments:
+            s     -- initial state
+            a     -- chosen action
+            r     -- received reward
+            s1    -- next state
+        '''
+        
+        if not s == 0:
+            alpha = self.alpha2
+            delta = r - self.QTD[s * self.num_actions + a]
+            self.QTD[s * self.num_actions + a] += alpha * delta
+        else:
+            alpha = self.alpha1
+            delta = r - self.QTD[s1 * self.num_actions + a1] if trace else r + self.QTD[s1 * self.num_actions + a1] - self.QTD[s * self.num_actions + a]
+            self.QTD[s * self.num_actions + a] += alpha * delta * (self.lam * trace) if trace else alpha * delta
+
 
         return None
     
+    def _Q_modelBased(self, s, a):
+
+        '''
+        Update the Q-value table of mb
+        Input arguments:
+            s     -- initial state
+            a     -- chosen action
+        '''
+
+        if s != 0:
+            self.QMB[s * self.num_actions + a] = self.QTD[s * self.num_actions + a]
+            return None
+
+        if self.transition_count_b > self.transition_count_c:
+            self.transition = self.transition_matrix[0]
+        else:
+            self.transition = self.transition_matrix[1]
+
+        if s == 0:
+            p_a = self.transition[a]
+            p_not_a = 1-p_a
+
+            maxQ_s2 = np.max(self.QTD[[2,3]]) 
+            maxQ_s3 = np.max(self.QTD[[4,5]])
+            self.QMB[s * self.num_actions + a] = p_a*maxQ_s2 +p_not_a*maxQ_s3
+
+        
+        return None
+    
+    def _Q_net(self, s, a):
+
+        '''
+        Update the Q-value table of combined td and mb
+        Input arguments:
+            s     -- state
+            a     -- chosen action
+
+        '''
+        s_a = s*self.num_actions+a
+
+        if s == 0:
+            self.Qnet[s_a] = self.w * self.QMB[s_a] + (1-self.w)*self.QTD[s_a]
+        else:
+            self.Qnet[s_a] = self.QTD[s_a]
+
+        return None
+
+    
+    def _policy(self, s):
+
+        '''
+        Agent's policy 
+        Input arguments:
+            s -- state
+        Output:
+            a -- index of action to be chosen
+        '''
+        exp_terms = np.zeros(2)
+        
+        for a in range(self.num_actions):
+            if s == 0:
+                beta = self.beta1
+                rep = self.last_a == a
+            else:
+                beta = self.beta2
+                rep = 0
+            s_a = s*self.num_actions+a
+
+            exp_terms[a] = np.exp(beta*(self.Qnet[s_a] + self.p * rep)) #qnet
+
+        policy =  exp_terms/ exp_terms.sum()
+
+        a = np.random.choice(np.arange(2),p=policy)
+        return a
+    
+    def get_next_state(self,s,a):
+        if s == 0:
+            p = [self.transition_matrix[0][a], 1-self.transition_matrix[0][a]]
+            new_state = np.random.choice([1,2], p=p)
+
+        return new_state
+
     def get_stay_probabilities(self):
 
         '''
@@ -357,8 +509,45 @@ class TwoStepAgent:
         Main simulation function
         Input arguments:
             num_trials -- number of trials to simulate
-        '''
-            
-        # complete the code
+        '''   
 
+        self._init_q_values()
+        self._init_history()
+
+        for _ in range(num_trials):
+            s1 = self.start_state
+            # choose action
+            a1  = self._policy(s1)
+
+            # get new state
+            r1=0
+            s2 = self.get_next_state(s1,a1)
+
+            if (s1==1 and a1==0) or (s1==2 and a1==1):
+                self.transition_count_b+=1
+            elif (s1==2 and a1==0) or (s1==1 and a1==1):
+                self.transition_count_c+=1
+
+            self.last_a = a1
+
+            # receive reward
+            a2 = self._policy(s2)
+            self._Q_td(s1, a1, r1, s2,a2,False)
+            p = self.rewards[s2]
+            r2 = np.random.choice((0,1), p=(1-p, p))
+
+            # learning
+            self._Q_td(s2, a2, r2, _,_,False)
+            self._Q_td(s1, a1, r2, s2,a2,True)
+
+            self._Q_modelBased(s1,a1)
+            self._Q_modelBased(s2,a2)
+
+            self._Q_net(s1,a1)
+            self._Q_net(s2,a2)
+
+            #update history
+            self._update_history(a1, s2, r2)
+            self.add_noise()
+            
         return None
